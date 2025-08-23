e36882043ed1899fbc4f4d1c2b6ea18b302c9a14
rageeshan <rageeshancr7@gmail.com>
2025-08-23 13:20:32 +0530

    DB Connect commit

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB Connected");
+    console.log("MongoDB Connected");
   } catch (error) {
-    console.error("❌ MongoDB Connection Failed:", error.message);
+    console.error("MongoDB Connection Failed:", error.message);
     process.exit(1);
   }
 };

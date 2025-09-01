512338a1a8f30a6128c725895207b1fc4569d4a3
rageeshan <rageeshancr7@gmail.com>
2025-09-01 11:23:52 +0530

    1st Sept CRUD done!!!

--- backend/src/controllers/sosController.js
+++ backend/src/controllers/sosController.js
@@ -24,7 +24,7 @@ export async function createSOS(req, res) {
     const savedSOS = await sos.save();
     res.status(201).json(savedSOS);
   } catch (error) {
-    console.error("Error on getAllsos Controller", error);
+    console.error("Error on createSOS Controller", error);
     res.status(500).json({ message: "Internal Server Error!" });
   }
 }
@@ -32,24 +32,46 @@ export async function createSOS(req, res) {
 export async function updateSOS(req, res) {
   try {
     const { name, age, number, location } = req.body;
-    const updatedSOS = await SOS.findByIdAndUpdate(req.params.id, {
-      name,
-      age,
-      number,
-      location,
-    });
+    const updatedSOS = await SOS.findByIdAndUpdate(
+      req.params.id,
+      {
+        name,
+        age,
+        number,
+        location,
+      },
+      { new: true }
+    );
 
-    if (!updatedSOS) return res.status(404).json({ message: "Note not found" });
+    if (!updatedSOS) return res.status(404).json({ message: "SOS not found" });
 
     res.status(200).json(updatedSOS);
     // res.status(200).json({ message: "SOS updated successfully!" });
   } catch (error) {
-    console.error("Error on getAllsos Controller", error);
+    console.error("Error on updateSOS Controller", error);
     res.status(500).json({ message: "Internal Server Error!" });
   }
 }
 
 export async function deleteSOS(req, res) {
-  // Delete user acc
-  res.status(200).send("You have deleted an acc");
+  try {
+    const deletedNote = await SOS.findByIdAndDelete(req.params.id);
+
+    if (!deletedNote) return res.status(404).json({ message: "SOS not found" });
+    res.status(200).json({ message: "SOS deleted successfully!" });
+  } catch (error) {
+    console.error("Error on deleteSOS Controller", error);
+    res.status(500).json({ message: "Internal Server Error!" });
+  }
+}
+
+export async function getSOSByID(req, res) {
+  try {
+    const sos = await SOS.findById(req.params.id);
+    if (!sos) return res.status(404).json({ message: "SOS not found" });
+    res.json(sos);
+  } catch (error) {
+    console.error("Error on getByID Controller", error);
+    res.status(500).json({ message: "Internal Server Error!" });
+  }
 }
--- backend/src/routes/sosRoute.js
+++ backend/src/routes/sosRoute.js
@@ -4,11 +4,14 @@ import {
   deleteSOS,
   getAllsos,
   updateSOS,
+  getSOSByID,
 } from "../controllers/sosController.js";
 
 const router = express.Router();
 
 router.get("/", getAllsos);
+
+router.get("/:id", getSOSByID);
 
 router.post("/", createSOS);
 

de8a73b39eae0c4a7eefd2f55e32d6845003f90f
rageeshan <rageeshancr7@gmail.com>
2025-09-19 13:36:23 +0530

    Homepage commit 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,15 @@
 import React from "react";
-import { Route } from "lucide-react";
+import { Routes, Route } from "react-router-dom";
 import Homepage from "../pages/Homepage";
 
 function App() {
-  return <Homepage />;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
